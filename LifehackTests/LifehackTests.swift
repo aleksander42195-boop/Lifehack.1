a68e13018943c79763e90dc7c8a4222e78d1f5a4
Aleksander blindheim <Aleksander42195@gmail.com>
2025-09-18 11:15:37 -0700

    Initial Commit

---
+++ LifehackTests/LifehackTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifehackTests.swift
+//  LifehackTests
+//
+//  Created by Aleksander Blindheim on 18/09/2025.
+//
+
+import Testing
+@testable import Lifehack
+
+struct LifehackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
